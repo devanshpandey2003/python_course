42b3b79e5bb7c0649179e85c380448786d7c52e4
devanshpandey2003 <devanshpandey8900@gmail.com>
2025-11-26 00:09:24 +0530

    1st Assignment

---
+++ basic_opration.py
@@ -0,0 +1,12 @@
+num1: int = int(input("Enter first number: "))
+num2: int = int(input("Enter second number: "))
+
+Addition: int = num1 + num2
+Substraction: int = num1 - num2
+Multiplication: int = num1 * num2
+Division: float = num1 / num2
+
+print("Addition: ", Addition)
+print("Substraction: ", Substraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
---
+++ personalized_greeting.py
@@ -0,0 +1,4 @@
+first_name: str = input("Enter your first name: ")
+last_name: str = input("Enter your last name: ")
+full_name: str = first_name + " " + last_name
+print(f"Hello {full_name}! Welcome to the pyhton programming!")
